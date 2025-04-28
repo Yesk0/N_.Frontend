64a2bf39982075b855ca2a507e680fedcbf0c7d8
Yesko <iskoaleksandor@gmail.com>
2025-04-28 19:44:29 +0500

    feat: add html and js files with initial code

--- week2/homework3.js
+++ week2/homework3.js
@@ -1,1 +1,46 @@
-//1
+//first task
+// let number = prompt("give me random number:");
+// if(!isNaN(number)){
+//     alert(`Your number: ${number}`);
+// }
+// else {
+//     alert("It's not a number");
+// }
+
+//second task
+// let a = +prompt('a?', '');
+// console.log(a) // string
+// a = +a
+// console.log(a) // number
+
+// switch(a){
+//     case 0: 
+//         alert( 0 );
+//         break;
+//     case 1:
+//         alert( 1 );
+//         break;
+//     case 2 || 3: 
+//         alert( '2,3' );
+//         break;
+// }
+
+//third task
+// let sum = 0;
+// for(let i = 1; i <= 100; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//fourth task
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//   }
+
+// let i = 0;
+// while(i < 3){
+//     alert( `number ${i}!` );
+//     i++;
+// }
+
+
